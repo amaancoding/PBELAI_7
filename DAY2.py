582a250e94aef89ad9856f721f2986113cfb2816
AMAAN <you@example.com>
2026-06-22 21:40:03 +0530

    1. DAY1 file is created

---
+++ DAY2.py
@@ -0,0 +1,42 @@
+#list
+my_list=[1,2,3,4,5]
+print(my_list)
+print(type(my_list))
+
+#prop. and method
+print(len(my_list))
+my_list.append(6)
+print(my_list)
+
+my_list.insert(0,0)
+print(my_list)
+
+my_list.extend([7,8,9])
+print(my_list)
+
+my_list.remove(3)
+print(my_list)
+
+my_list.pop()
+print(my_list)
+
+#properties
+#ordered
+my_list[7]=10
+print(my_list)
+#duplicasy and heterogeneity
+f
+print(FUN)
+
+##tuple
+abc=(1,2,3,4,5)
+print(abc)
+print(type(abc))
+
+#set
+my_set= {1,2,3,4,5}
+
+#dictionary
+
+
+#string
